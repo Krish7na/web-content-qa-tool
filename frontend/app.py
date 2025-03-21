b62c84554b421ab69458845319baa53c609bf509
KRISHNA SHARMA <2021ad4kr@mitsgwl.ac.in>
2025-03-21 18:07:48 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "https://web-cont-qa-tool.onrender.com"
+API_URL = "https://web-content-qa-tool-mcls.onrender.com"
 
 st.title("Web Content Q&A Tool (Mistral-7B)")
 
